66dedb7e5d13af9a037d0fe95054c58329aaab83
Eduardo Ascencio <EduardoAscencio@outlook.es>
2026-06-01 11:39:53 -0500

    Implementacion de endpoint de prueba

---
+++ src/main/java/com/trinity/financial_api/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.trinity.financial_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("API funcionando correctamente");
+    }
+}
